4f5eb38fd6344a6dd0806dc1efab0bdfa14b947a
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-23 18:59:36 +0700

    refactor: add comments

--- app/classes/class.service.js
+++ app/classes/class.service.js
@@ -19,13 +19,17 @@ const findAllClasses = async (res) => {
 
 const findClass = async (req, res) => {
     try {
+        // get id (class id) from url
         const { id: classId } = req.params;
         const class_ = await Class.findById(classId);
+        // create message for class not found
         const message = setAttributeMessage(
             responseMessages.classNotFound,
             classId
         );
+        // if class not found, the errors are gonna be send to catch error
         await handleNotFound(class_, message);
+        // if class found, send to client
         return successResponse(res, {
             data: class_,
         });
@@ -36,9 +40,13 @@ const findClass = async (req, res) => {
 
 const createClass = async (req, res) => {
     try {
+        // get client input after validation middleware
         const { name, description } = req.body;
+        // handle get unique code
         const code = await getAvalaibleCode();
+        // create new class
         const newClass = await Class.create({ name, description, code });
+        // and send to client
         return createdResponse(res, {
             data: newClass,
         });
