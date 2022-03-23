5f48b729eb0758738c4bcce845ea84a35cf4ebcc
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-23 09:28:58 +0700

    refactor: comment in class controller

--- app/classes/class.controller.js
+++ app/classes/class.controller.js
@@ -6,7 +6,7 @@ import classService from './class.service.js';
  * @param {Object} res
  * @desc Find all classes
  * @method GET
- * @access PUBLIC/PRIVATE
+ * @access PUBLIC
  */
 export const findAllClasses = async (_, res) => {
     return await classService.findAllClasses(res);
