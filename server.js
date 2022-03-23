0509e94b068146d0a0fc31d1571072ad1f6ce198
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-23 07:33:12 +0700

    refactor: getEnv function

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { getEnv, isDevelopment } from './utils'
-dotenv.config()
+import express from 'express';
+import colors from 'colors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { getEnv, isDevelopment } from './utils';
+dotenv.config();
 
-const app = express()
-const PORT = getEnv('PORT', 5000)
+const app = express();
+const PORT = getEnv('PORT', 5000);
 
 app.listen(PORT, () => {
     // development mode
     if (isDevelopment())
-        console.log(`Server is running at http://localhost:${PORT}`)
+        console.log(`Server is running at http://localhost:${PORT}`);
     // production mode
-    console.log(`Server is running in production mode on port ${PORT}`)
-})
+    console.log(`Server is running in production mode on port ${PORT}`);
+});
--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,7 @@
-export const getEnv = (key = '', defaultValue = '') => {
+export const getEnv = (key = '', defaultValue = null) => {
     return process.env[key] || defaultValue;
 };
 
 export const isDevelopment = () => {
     return getEnv('NODE_ENV', 'development') === 'development';
-};
-
-export const log = (message = '') => {
-    console.log(message);
 };
