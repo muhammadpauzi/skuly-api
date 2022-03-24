5e8d2097e12a4eff9e50f94933be35906b53679e
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-24 09:07:39 +0700

    add authorized response

--- utils/response.js
+++ utils/response.js
@@ -1,4 +1,4 @@
-import { CREATED, SUCCESS } from '../constants/statusCode.js';
+import { CREATED, SUCCESS, UNAUTHORIZED } from '../constants/statusCode.js';
 
 export const createdResponse = (res, data = {}) => {
     data.success = true;
@@ -13,6 +13,16 @@ export const successResponse = (res, data = {}, statusCode = SUCCESS) => {
 };
 
 export const notFoundResponse = (res, data = {}, statusCode = NOT_FOUND) => {
+    data.success = false;
+    data.statusCode = statusCode;
+    return res.status(statusCode).json(data);
+};
+
+export const unauthorizedResponse = (
+    res,
+    data = {},
+    statusCode = UNAUTHORIZED
+) => {
     data.success = false;
     data.statusCode = statusCode;
     return res.status(statusCode).json(data);
