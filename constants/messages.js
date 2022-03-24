784c29ad4497e80ff2c1fe0128ac8906f9b04cab
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-24 08:27:15 +0700

    update messages

--- constants/messages.js
+++ constants/messages.js
@@ -2,6 +2,13 @@ export const responseMessages = {
     classNotFound: 'The class with the id :attribute not found.',
 };
 
+export const authMessages = {
+    userRegistered: 'The user has been successfully registered.',
+};
+
 export const validationMessages = {
     required: 'The :attribute field is required.',
+    email: 'The :attribute must be a valid email address.',
+    alreadyRegistered: 'The :attribute already registered.',
+    alphaNum: 'The :attribute must only contain letters and numbers.',
 };
