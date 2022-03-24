6f8fdf03a050cf6118a715cd08529065accb940e
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-24 09:03:44 +0700

    update toJSON class model

--- app/classes/class.model.js
+++ app/classes/class.model.js
@@ -25,4 +25,13 @@ const classSchema = mongoose.Schema(
     { timestamps: true }
 );
 
+// hide code
+classSchema.set('toJSON', {
+    transform: function (doc, ret, opt) {
+        delete ret['code'];
+        delete ret['__v'];
+        return ret;
+    },
+});
+
 export default mongoose.model('Class', classSchema);
