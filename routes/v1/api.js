ca36323a636f2d2c0e9e37619084a70f121174af
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-24 08:26:38 +0700

    add route for auth endpoints

--- routes/v1/api.js
+++ routes/v1/api.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { classRouter } from '../../app/classes/index.js';
+import { authRouter } from '../../app/auth/index.js';
 
 const router = express.Router();
 
 // classes base route
 router.use(`/v1/classes`, classRouter);
+router.use(`/v1/auth`, authRouter);
 
 export default router;
