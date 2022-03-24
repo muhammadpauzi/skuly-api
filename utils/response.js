19fc403a597a1ee2cd52c7d07cdbbfb4c52a0e9b
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-24 18:55:08 +0700

    add clientErrorResponse function

--- utils/response.js
+++ utils/response.js
@@ -1,4 +1,9 @@
-import { CREATED, SUCCESS, UNAUTHORIZED } from '../constants/statusCode.js';
+import {
+    CLIENT_ERROR,
+    CREATED,
+    SUCCESS,
+    UNAUTHORIZED,
+} from '../constants/statusCode.js';
 
 export const createdResponse = (res, data = {}) => {
     data.success = true;
@@ -22,6 +27,16 @@ export const unauthorizedResponse = (
     res,
     data = {},
     statusCode = UNAUTHORIZED
+) => {
+    data.success = false;
+    data.statusCode = statusCode;
+    return res.status(statusCode).json(data);
+};
+
+export const clientErrorResponse = (
+    res,
+    data = {},
+    statusCode = CLIENT_ERROR
 ) => {
     data.success = false;
     data.statusCode = statusCode;
